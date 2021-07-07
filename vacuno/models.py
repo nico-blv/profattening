ae34c9758cfaa1d97710c6b4540692a116703da3
nico-blv <nicolas.mino06@inacapmail.cl>
2021-07-06 23:16:49 -0400

    Se agrega prototipo

---
+++ vacuno/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Vaca(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    peso = models.TextField(verbose_name="Peso")
+    raza = models.TextField(verbose_name="Raza", default="Vaca")
+    sexo = models.TextField(verbose_name="Sexo")
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=["-created"]
